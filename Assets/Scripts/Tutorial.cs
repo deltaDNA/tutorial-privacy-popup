04967d45f780036f8c69d3295b017582c1395f96
LaurieMcCulloch <lauriemcculloch@hotmail.com>
2020-12-10 23:14:57 +0000

    deltaDNA SDK v4.13.4 Added

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DeltaDNA;
+
+public class Tutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //DDNA.Instance.ClearPersistentData();
+        DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
+        DDNA.Instance.StartSDK();
+        
+    }
+}
